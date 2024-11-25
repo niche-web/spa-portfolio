ed6dae3ff31813a4c472833a8fb0c029514b00d9
nicheweb <norgerojascerulia@gmail.com>
2024-11-25 12:01:23 +0100

    first layout commit

---
+++ src/layout/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="heading-xl">
+        Nice to meet you! I&rsquo;m <a href="#">Nicheweb</a>
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
