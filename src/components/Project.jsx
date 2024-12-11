91469abb27e9065a76e6962bbfef7036306782e3
nicheweb <norgerojascerulia@gmail.com>
2024-12-11 16:31:22 +0100

    Added margin bottom to section projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -16,6 +16,10 @@ const Project = ({ number, title, tagsList }) => {
         alt="Project Thumbnail image"
         className="project__img"
       />
+      <div className="project__description">
+        <h4 className="project__title heading heading--m">{title}</h4>
+        <p className="project__tags paragraph">{tags}</p>
+      </div>
       <div className="project__links">
         <Button text="view project" classes="project__live-link" href="#">
           view project
@@ -24,8 +28,6 @@ const Project = ({ number, title, tagsList }) => {
           view code
         </Button>
       </div>
-      <h4 className="project__title heading heading--m">{title}</h4>
-      <p className="project__tags paragraph">{tags}</p>
     </li>
   );
 };
