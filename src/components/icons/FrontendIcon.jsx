8a203f2787c552a4f4ae13f789d34e612e957836
nicheweb <norgerojascerulia@gmail.com>
2024-12-12 14:55:25 +0100

    Fourth attemp icons

--- src/components/icons/FrontendIcon.jsx
+++ src/components/icons/FrontendIcon.jsx
@@ -1,9 +1,9 @@
-import iconFrontend from "/icons/icon-frontend-mentor.svg";
+// import iconFrontend from "/icons/icon-frontend-mentor.svg";
 
 export const FrontendIcon = ({ classes }) => {
   return (
     <svg className={classes}>
-      <use href={iconFrontend + "#icon-frontend"} />
+      <use href="/icons/icon-frontend-mentor.svg#icon-frontend" />
     </svg>
   );
 };
