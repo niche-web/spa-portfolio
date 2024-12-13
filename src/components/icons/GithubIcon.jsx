405e09eea29efa86572fc55079be643346543d6c
nicheweb <norgerojascerulia@gmail.com>
2024-12-13 10:19:39 +0100

    Fix icons problems- fithh attemp

--- src/components/icons/GithubIcon.jsx
+++ src/components/icons/GithubIcon.jsx
@@ -1,9 +1,9 @@
-import iconGithub from "../../assets/icons/sprite.svg#icon-github";
+// import iconGithub from "../../assets/icons/sprite.svg#icon-github";
 
 export const GithubIcon = () => {
   return (
     <svg className="icon">
-      <use xlinkHref={iconGithub}></use>
+      <use xlinkHref="../../assets/icons/sprite.svg#icon-github"></use>
     </svg>
   );
 };
