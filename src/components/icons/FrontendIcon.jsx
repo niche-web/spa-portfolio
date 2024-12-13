1b5b2a84d333000b504e1c44bbf323a5e9415ff6
nicheweb <norgerojascerulia@gmail.com>
2024-12-13 13:27:48 +0100

    Fix icons problems- 10th attemp

--- src/components/icons/FrontendIcon.jsx
+++ src/components/icons/FrontendIcon.jsx
@@ -1,8 +1,8 @@
 import iconFrontend from "../../assets/icons/sprite.svg#icon-frontend-mentor";
 
-export const FrontendIcon = () => {
+export const FrontendIcon = ({ classes }) => {
   return (
-    <svg className="icon">
+    <svg className={classes}>
       <use xlinkHref={iconFrontend}></use>
     </svg>
   );
